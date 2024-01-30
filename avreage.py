2ccf7346d3f7ea45e72f8f8bf5c61e6359a58727
hudaazeem440 <156990131+hudaazeem440@users.noreply.github.com>
2024-01-30 15:57:08 +0500

    Add files via upload

---
+++ avreage.py
@@ -0,0 +1,11 @@
+#this_is_the_average
+a=int(input("enter the first no here"))
+b=int(input("enter the second no here"))
+total=(a+b)/2
+print("the average is",total)
+#this_is_the_area_of_the_circle
+radius=float(input("enter the radius"))
+pi=float(3.14)
+area_of_the_circle=pi*radius*2
+print("area of the circle is",area_of_the_circle)
+       
